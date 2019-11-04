2292aea9d5cc0b22b4c240d683daddaf0dc2e58b
lastWhi <1040044589@qq.com>
2019-11-04 17:15:04 +0800

    文章获取及渲染完成

--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -1,13 +1,85 @@
 <template>
-  <div>文章列表</div>
+  <el-table
+    :data="postLists"
+    style="width: 100%;margin-top:30px">
+    <el-table-column
+      label="Id"
+      width="40">
+      <template slot-scope="item">
+        <span>{{item.row.id}}</span>
+      </template>
+    </el-table-column>
+    <el-table-column
+      label="标题"
+      width="500">
+      <template slot-scope="item">
+        <span>{{item.row.title}}</span>
+      </template>
+    </el-table-column>
+     <el-table-column
+      label="缩略图"
+      width="200">
+      <template slot-scope="item">
+        <span>
+            <img class="thumbnail" :src="item.row.cover[0].url" alt="">
+        </span>
+      </template>
+    </el-table-column>
+    <el-table-column label="操作">
+      <template slot-scope="scope">
+        <el-button
+          size="mini"
+          @click="handleEdit(scope.$index, scope.row)">编辑</el-button>
+        <el-button
+          size="mini"
+          type="danger"
+          @click="handleDelete(scope.$index, scope.row)">删除</el-button>
+      </template>
+    </el-table-column>
+  </el-table>
 </template>
 
 <script>
-export default {
+  export default {
+    data() {
+      return {
+          postLists:[]
+      }
+    },
+    created(){
+        this.getPostsList()
+    },
+    methods: {
+      handleEdit(index, row) {
+        console.log(index, row);
+      },
+      handleDelete(index, row) {
+        console.log(index, row);
+      },
 
-}
+       //获取全部文章
+      getPostsList(){
+          this.$axios({
+              url:'/post',
+              method:'get',
+          }).then(res=>{
+              let {data}=res.data;
+
+              console.log(data);
+              this.postLists=data;
+          })
+      }
+    }
+  }
 </script>
 
-<style>
-
+<style lang="less" scoped>
+el-table{
+    margin-top: 30px;
+}
+.thumbnail{
+    width: 100%;
+    height: 100px;
+    object-fit: cover;
+}
 </style>
